47c0242c9176006228a29b5f14c64e49e13e5b10
FanJunling <32149090+FanJunling@users.noreply.github.com>
2019-06-04 16:25:05 +0800

    Update myGroovyscript.groovy

--- myGroovyscript.groovy
+++ myGroovyscript.groovy
@@ -4,5 +4,5 @@ import jenkins.model.*
 jenkins = Jenkins.instance
 
 def log = readFile("/home/jenkins/workspace/fantest/version.csv") 
-def version = log.text.toString()
+def version = log.text
 println version
